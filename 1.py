460a910d616c965401a9c73884d4e5a097d10df7
Bububinks009 <hounsoufortune3@gmail.com>
2025-05-21 16:55:09 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,1 @@
-# Écrire un programme qui demande un nom à l’utilisateur et affiche "Bonjour, [nom] 
 
-nom_utilisateur = input ( " Entrer votre nom d'utilisateur : ")    # demande du nom d'utilisateur et son entrer
-print ( " Bonjour, " , nom_utilisateur + "!")                      # affichage de Bonjour , nom d'utilisateur
