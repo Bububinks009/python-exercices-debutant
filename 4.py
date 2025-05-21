74f07c807a190656ef90856427362c7eaf7d9eda
Bububinks009 <hounsoufortune3@gmail.com>
2025-05-21 16:39:04 +0100

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,8 @@
+# Afficher si un nombre entré est pair ou impair.
+
+n = int(input( " Entrer un nombre entier n = "))
+if (n%2) == 0 :
+    print( " n est pair ")
+else:
+    print( " n est impair ")
+    
