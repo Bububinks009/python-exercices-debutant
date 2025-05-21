a1c43211d6a9f051e6c7168bbf777adee855b71b
Bububinks009 <hounsoufortune3@gmail.com>
2025-05-21 17:04:29 +0100

    Update and rename 2.py to 007_Vérification mot.py

---
+++ 007_Vérification mot.py
@@ -0,0 +1,6 @@
+# Créer une boucle qui demande un mot jusqu’à ce que l’utilisateur tape << exit >>.
+
+mot = ""  # creation d une variable mot
+while mot != "exit":  # tant que mot different de exit
+   mot = input("Entrer un mot :")  # afficher : Enter un mot
+print("Exat!")  # afficher Exat! si le mot est vrai
--- 2.py
+++
@@ -1,6 +0,0 @@
-# Calculer l’aire d’un rectangle à partir de la longueur et la largeur saisies.
-
-L = float(input ( " L = "))     # entrer la valeur de L qui ne prend que des réels
-l = float(input ( " l = "))     # entrer la valeur de l qui ne prend que des réels
-A = L*l                         # calcul de A=L*l
-print( " A = " , A )            # affichage du résultat sous forme A=.....
